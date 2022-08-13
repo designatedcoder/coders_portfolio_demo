92b2094e8e5a2c6616842449910e772c2a533588
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-08-12 21:01:17 -0400

    Landing Page Livewire Component

---
+++ coders_portfolio/app/Http/Livewire/LandingPage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.landing-page')
+            ->layout('layouts.guest');
+    }
+}
